92a3262a58ed89d5d15f55648f7a7f2314d29441
KristineKem <kristine.manina@gmail.com>
2023-05-10 18:58:30 +0300

    Fligth planner started

---
+++ FligthPlanner/FlightPlanner/FlightPlanner/Controllers/AdminApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("admin-api")]
+    [ApiController]
+    [Authorize]
+    public class AdminApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("fligths/{id}")]
+        public IActionResult GetFlight(int id)
+        {
+            return Ok("returnning flights");
+        }
+    }
+}
